70838df6e46194d2e5684b6c1013369da6ebf34f
Jang <87692888+JangSeongwon@users.noreply.github.com>
2024-10-21 16:37:10 +0900

    Update HapticOriPublisher.cs

--- I2CPS/HapticOriPublisher.cs
+++ I2CPS/HapticOriPublisher.cs
@@ -8,6 +8,7 @@ namespace RosSharp.RosBridgeClient
        
         private MessageTypes.Geometry.Pose message;
         public int Pose_mode;
+        public Vector3 Saved_Ori_i;
         public Vector3 Saved_Ori;
 
         protected override void Start()
@@ -34,42 +35,58 @@ namespace RosSharp.RosBridgeClient
             {
                 if (Pose_mode == 0)
                 {
+                    print("Saving current Orientation of Haptic");
                     Pose_mode = 1;
-                    print("Saving current Orientation");
+                    return;
                 }
                 else if (Pose_mode == 1)
                 {
-                    Pose_mode = 2;
                     print("Robot's Orientation Modification");
+                    Pose_mode = 2;
+                    return;
                 }
                 else if (Pose_mode == 2)
+                {
+                    print("Finish Orientation Modification");
                     Pose_mode = 0;
+                }
+                return;
             }
         }
 
         private void InitializeMessage()
         {
             message = new MessageTypes.Geometry.Pose();
-           
         }
 
         private void Saving_Initial_Ori()
         {
-            Matrix4x4 localToWorldMatrix = PublishedTransform.localToWorldMatrix;
-            Quaternion rotation = Quaternion.LookRotation(localToWorldMatrix.GetColumn(2), localToWorldMatrix.GetColumn(1));
-            Saved_Ori = rotation.eulerAngles;
-            //print($"Saving current Orientation, {Saved_Ori}");
+            Quaternion rotation = PublishedTransform.rotation;
+            Saved_Ori_i = rotation.eulerAngles;
 
+            if (Saved_Ori_i.x >= 180)
+                Saved_Ori.x = Saved_Ori_i.x - 360;
+            else
+                Saved_Ori.x = Saved_Ori_i.x;
+            if (Saved_Ori_i.y >= 180)
+                Saved_Ori.y = Saved_Ori_i.y - 360;
+            else
+                Saved_Ori.y = Saved_Ori_i.y;
+            if (Saved_Ori_i.z >= 180)
+                Saved_Ori.z = Saved_Ori_i.z - 360;
+            else
+                Saved_Ori.z = Saved_Ori_i.z;
+            //print($"Saving current Orientation, {Saved_Ori}");
         }
 
         private void UpdateMessage()
         {
-           
             // Get position and rotation matrices from the Unity Transform
-            Matrix4x4 localToWorldMatrix = PublishedTransform.localToWorldMatrix;
-
+            //Matrix4x4 localToWorldMatrix = PublishedTransform.localToWorldMatrix;
             // Extract rotation from the matrices
-            Quaternion rotation = Quaternion.LookRotation(localToWorldMatrix.GetColumn(2), localToWorldMatrix.GetColumn(1));
+            //Quaternion rotation = Quaternion.LookRotation(localToWorldMatrix.GetColumn(2), localToWorldMatrix.GetColumn(1));
+
+            Quaternion rotation = PublishedTransform.rotation;
 
             // Convert Quaternion to Euler angles
             Vector3 euler = rotation.eulerAngles;
@@ -82,12 +99,22 @@ namespace RosSharp.RosBridgeClient
 
         private static void GetGeometryEuler(Vector3 euler, MessageTypes.Geometry.Quaternion geometryQuaternion,Vector3 Saved_Ori)
         {
-            
+
             // Set the Quaternion in the message
-            geometryQuaternion.x = euler.x - Saved_Ori.x;
-            geometryQuaternion.y = euler.y - Saved_Ori.y;
-            geometryQuaternion.z = euler.z - Saved_Ori.z;
+            if (euler.x >= 180)
+                geometryQuaternion.x = euler.x - 360 - Saved_Ori.x;
+            else
+                geometryQuaternion.x = euler.x - Saved_Ori.x;
+            if (euler.y >= 180)
+                geometryQuaternion.y = euler.y - 360 - Saved_Ori.y;
+            else
+                geometryQuaternion.y = euler.y - Saved_Ori.y;
+            if (euler.z >= 180)
+                geometryQuaternion.z = euler.z - 360 - Saved_Ori.z;
+            else
+                geometryQuaternion.z = euler.z - Saved_Ori.z;
             geometryQuaternion.w = 0.0f;
+            print($"See Haptic Orientation, X: {euler.x}, {Saved_Ori.x}, Y: {euler.y}, {Saved_Ori.y}, Z: {euler.z}, {Saved_Ori.z}");
             print($"See Haptic Orientation, {geometryQuaternion.x}, {geometryQuaternion.y}, {geometryQuaternion.z}");        
         }
     }

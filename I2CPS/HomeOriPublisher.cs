8defc2b3e82393f68304959e6f02fd58bb19e092
Jang <87692888+JangSeongwon@users.noreply.github.com>
2024-10-31 11:14:07 +0900

    Create HomeOriPublisher.cs

---
+++ I2CPS/HomeOriPublisher.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class HomeOriPublisher : UnityPublisher<MessageTypes.Geometry.Pose>
+    {
+        private MessageTypes.Geometry.Pose message_home;
+        public HapticOriPublisher HapticOriPublisher;
+        public int Home_mode;
+
+        protected override void Start()
+        {
+            base.Start();
+            Home_mode = HapticOriPublisher.Orientation_mode;
+            InitializeMessage();
+        }
+
+        private void FixedUpdate()
+        {
+            if (Home_mode == 2)
+            {
+                SendMessageHome();
+            }
+        }
+
+        private void InitializeMessage()
+        {
+            message_home = new MessageTypes.Geometry.Pose();
+        }
+
+        private void SendMessageHome()
+        {
+            Vector3 euler = new Vector3 ( 0, 0, 0);
+            GetGeometryEuler(euler, message_home.orientation);
+            Publish(message_home);
+        }
+
+        private static void GetGeometryEuler(Vector3 euler, MessageTypes.Geometry.Quaternion geometryQuaternion)
+        {
+            if (euler.x >= 180)
+                geometryQuaternion.x = euler.x - 360;
+            else
+                geometryQuaternion.x = euler.x;
+            if (euler.y >= 180)
+                geometryQuaternion.y = euler.y - 360;
+            else
+                geometryQuaternion.y = euler.y;
+            if (euler.z >= 180)
+                geometryQuaternion.z = euler.z - 360;
+            else
+                geometryQuaternion.z = euler.z;
+            geometryQuaternion.w = 0.0f;
+            print($"See Haptic Orientation, X: {euler.x}, Y: {euler.y}, Z: {euler.z}");
+            print($"See Haptic Orientation, {geometryQuaternion.x}, {geometryQuaternion.y}, {geometryQuaternion.z}");        
+
+        }
+    }
+}
